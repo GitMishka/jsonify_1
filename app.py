131669193201c5b2657a232e2665e00287a94938
Misha <mishagrigoryan@gmail.com>
2023-09-09 20:35:27 -0400

    adjustments

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, request, send_from_directory, render_template_string
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-class UserData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    age = db.Column(db.Integer)
+# Ensure upload directory exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    results = UserData.query.all()
-    return jsonify([{"id": result.id, "name": result.name, "age": result.age} for result in results])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-if __name__ == "__main__":
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser submits an empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'File uploaded successfully'
+
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template_string('''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    <h2>Existing Files:</h2>
+    {% for file in files %}
+    <a href="{{ url_for('download_file', name=file) }}">{{ file }}</a><br>
+    {% endfor %}
+    ''', files=files)
+
+@app.route('/uploads/<name>', methods=['GET'])
+def download_file(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+
+if __name__ == '__main__':
     app.run(debug=True)
