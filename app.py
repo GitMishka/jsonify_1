ec221b971071fad8eb0e04321437fdd66f41b8f2
Misha <mishagrigoryan@gmail.com>
2023-09-09 20:43:59 -0400

    rearranging

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, send_from_directory, render_template_string
+from flask import Flask, render_template, request, send_from_directory
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
@@ -7,7 +7,6 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Ensure upload directory exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -17,11 +16,9 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             return 'No file part'
         file = request.files['file']
-        # if user does not select file, browser submits an empty part without filename
         if file.filename == '':
             return 'No selected file'
         if file and allowed_file(file.filename):
@@ -30,19 +27,7 @@ def upload_file():
             return 'File uploaded successfully'
 
     files = os.listdir(UPLOAD_FOLDER)
-    return render_template_string('''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    <h2>Existing Files:</h2>
-    {% for file in files %}
-    <a href="{{ url_for('download_file', name=file) }}">{{ file }}</a><br>
-    {% endfor %}
-    ''', files=files)
+    return render_template('upload.html', files=files)
 
 @app.route('/uploads/<name>', methods=['GET'])
 def download_file(name):
